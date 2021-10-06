689e82c61a4e4371dac7a9e2bf21376e588c38d2
Paul Anthony McGowan <xhostcom@gmail.com>
2021-10-06 14:57:04 +0100

    Update

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,5 +1,4 @@
 require 'test_helper'
-
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
@@ -11,4 +10,17 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(self.response.body)
     assert_equal @user.email, json_response['email']
   end
+  test 'should create user' do
+    assert_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: 'test@test.org', password: '123456' } }, as: :json
+    end
+    assert_response :created
+  end
+  test 'should not create user with taken email' do
+    assert_no_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: @user.email, password: '123456' } }, as: :json
+    end
+    assert_response :unprocessable_entity
+  end
+  
 end
