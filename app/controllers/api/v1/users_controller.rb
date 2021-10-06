f3d8e8d523e5a6bdaec7bb2a3ab7b40f5152a436
Paul Anthony McGowan <xhostcom@gmail.com>
2021-10-06 15:40:10 +0100

    Set User

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update]
   # POST /users
   def create
     @user = User.new(user_params)
@@ -8,11 +9,25 @@ class Api::V1::UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
-
+  # GET /users/1
+  def show
+    render json: @user
+  end
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
 
   private
   # Only allow a trusted parameter "white list" through.
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
